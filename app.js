8f2854a7d14bf0c7c8c1a1d7f0bb4e8813233374
thiagoods <t.oliveira@outlook.com>
2018-04-19 23:56:05 +0200

    first homework - defining project estructure

---
+++ app.js
@@ -0,0 +1,8 @@
+import config from './config';
+import { User, Product } from './models';
+
+const user = new User();
+const product = new Product();
+
+console.log(config.name);
+console.log('============================');
